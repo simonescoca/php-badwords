dde7730c36073038419e68556ffa6bfa0516d64a
simonescoca <simone.scoca@icloud.com>
2023-06-30 16:22:42 +0200

    added form

--- index.php
+++ index.php
@@ -6,14 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
-    Gestire il tutto con due file diversi. Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
-    Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
-    stampare a schermo il paragrafo e la sua lunghezza
-    stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi tutte le occorrenze della parola da censurare -->
-    <?php
-        $prova = "ciao";
-        echo $prova;
-    ?>
+    <form action="./server.php" method="GET">
+        <textarea name="paragrafo" id="" cols="30" rows="10" placeholder="Scrivi un paragrafo"></textarea>
+        <input type="text" name="censura" id="" placeholder="Parola da censurare"> <!-- parola da censurare -->
+        <button type="submit">INVIA AL SERVER</button>
+    </form>
 </body>
 </html>
